b5b91b16cf40102d95b58b7d78ca8a7e57a357d7
Wisarmy Gao <wisarmy@gamil.com>
2014-08-12 11:00:09 +0800

    project init
    none

---
+++ src/com/musicshow/client/MainActivity.java
@@ -0,0 +1,87 @@
+package com.musicshow.client;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.musicshow.client.fragment.TopicListFragment;
+import com.musicshow.client.fragment.TopicListFragment.listViewUpdating;
+import com.musicshow.widget.SlideView;
+import com.musicshow.widget.SlideView.OnSlideListener;
+
+public class MainActivity extends FragmentActivity implements listViewUpdating, OnItemClickListener, OnClickListener,
+OnSlideListener {
+
+	ListView mListView;
+    private static final String TAG = "MainActivity";
+    private SlideView mLastSlideViewWithStatusOn;
+    
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.topic_list_fragment);
+
+        // Check whether the activity is using the layout version with
+        // the fragment_container FrameLayout. If so, we must add the first fragment
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create an instance of ExampleFragment
+            TopicListFragment firstFragment = new TopicListFragment();
+
+            // In case this activity was started with special instructions from an Intent,
+            // pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+	}
+
+	@Override
+	public void bindListData(ListView listView) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSlide(View view, int status) {
+        Log.e(TAG, "onSlide v=" + status);
+        if (mLastSlideViewWithStatusOn != null && mLastSlideViewWithStatusOn != view) {
+            mLastSlideViewWithStatusOn.shrink();
+        }
+
+        if (status == SLIDE_STATUS_ON) {
+            mLastSlideViewWithStatusOn = (SlideView) view;
+        }
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+        if (v.getId() == R.id.holder) {
+            Log.e(TAG, "onClick v=" + v);
+        }
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+        Log.e(TAG, "onItemClick position=" + position);
+		
+	}
+
+}
---
+++ src/com/musicshow/client/fragment/TopicListFragment.java
@@ -0,0 +1,188 @@
+package com.musicshow.client.fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.musicshow.client.R;
+import com.musicshow.widget.SlideView;
+import com.musicshow.widget.SlideView.OnSlideListener;
+
+
+public class TopicListFragment extends ListFragment{
+	listViewUpdating mCallback;
+	ListView mListView;
+    private List<MessageItem> mMessageItems = new ArrayList<MessageItem>();
+    static String[] Headlines = {
+        "Article One",
+        "Article Two"
+    };
+    public interface listViewUpdating {
+        public void bindListData(ListView listView);
+    }
+    
+
+    public void onCreate(Bundle savedInstanceState) {
+    	
+    	super.onCreate(savedInstanceState);
+        
+        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
+               android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+
+        // Create an array adapter for the list view, using the Ipsum headlines array
+        //setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Headlines));
+    	//mCallback.bindListData(getListView());
+        bindListData();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.listview_compat, container, false);
+    	//return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    	Log.i("Trace2",     getClass().getName()+ "->" +Thread.currentThread().getStackTrace()[2].getMethodName());
+    
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    	Log.i("Trace2",     getClass().getName()+ "->" +Thread.currentThread().getStackTrace()[2].getMethodName());
+    	try {
+            mCallback = (listViewUpdating) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement listViewUpdating");
+        }
+    }
+    
+    
+    
+    public void bindListData(){
+
+        for (int i = 0; i < 20; i++) {
+            MessageItem item = new MessageItem();
+            if (i % 3 == 0) {
+                item.iconRes = R.drawable.default_qq_avatar;
+                item.title = "腾讯新闻";
+                item.msg = "青岛爆炸满月：大量鱼虾死亡";
+                item.time = "晚上18:18";
+            } else {
+                item.iconRes = R.drawable.wechat_icon;
+                item.title = "微信团队";
+                item.msg = "欢迎你使用微信";
+                item.time = "12月18日";
+            }
+            mMessageItems.add(item);
+        }
+        setListAdapter(new SlideAdapter());
+        
+	}
+	
+	 public class MessageItem {
+	        public int iconRes;
+	        public String title;
+	        public String msg;
+	        public String time;
+			public SlideView slideView;
+
+	    }
+	    
+	    private static class ViewHolder {
+	        public ImageView icon;
+	        public TextView title;
+	        public TextView msg;
+	        public TextView time;
+
+	        ViewHolder(View view) {
+	            icon = (ImageView) view.findViewById(R.id.icon);
+	            title = (TextView) view.findViewById(R.id.title);
+	            msg = (TextView) view.findViewById(R.id.msg);
+	            time = (TextView) view.findViewById(R.id.time);
+	        }
+	    }
+	    
+	    private class SlideAdapter extends BaseAdapter {
+
+	        private LayoutInflater mInflater;
+
+	        SlideAdapter() {
+	            super();
+	            mInflater = getActivity().getLayoutInflater();
+	        }
+
+	        @Override
+	        public int getCount() {
+	        	Log.i("size", mMessageItems.size()+"");
+	        	
+	            return mMessageItems.size();
+	        }
+
+	        @Override
+	        public Object getItem(int position) {
+	        	Log.i("position", position+"");
+	            return mMessageItems.get(position);
+	        }
+
+	        @Override
+	        public long getItemId(int position) {
+	            return position;
+	        }
+
+			@SuppressLint("InflateParams") 
+			@Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+	            ViewHolder holder;
+	            SlideView slideView = (SlideView) convertView;
+	            if (convertView == null) {
+//	            	convertView = mInflater.inflate(R.layout.list_item, null);
+//	                holder = new ViewHolder(convertView);
+//	                convertView.setTag(holder);
+	                
+	                
+	                View itemView = mInflater.inflate(R.layout.topic_list_item, null);
+
+	                slideView = new SlideView(getActivity());
+	                slideView.setContentView(itemView);
+
+	                holder = new ViewHolder(slideView);
+	                slideView.setOnSlideListener((OnSlideListener) getActivity());
+	                slideView.setTag(holder);
+	            } else {
+//	                holder = (ViewHolder) convertView.getTag();
+	                holder = (ViewHolder) slideView.getTag();
+	            }
+	            
+	            
+	            MessageItem item = mMessageItems.get(position);
+	            item.slideView = slideView;
+	            item.slideView.shrink();
+	            holder.icon.setImageResource(item.iconRes);
+	            holder.title.setText(item.title);
+	            holder.msg.setText(item.msg);
+	            holder.time.setText(item.time);
+
+	            return slideView;
+	        }
+
+	    }
+    
+}
